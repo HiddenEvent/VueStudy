ae8e8acf6721853f0dbdd76d83ec3e17e77fb602
RichardKim <hiddeneventor@gmail.com>
2021-06-12 11:11:03 +0900

    타입스크립트 테스트

--- vue-start/src/App.vue
+++ vue-start/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-
+      하이
   </div>
 </template>
 
@@ -12,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
