4eb3364f2611bd9f8b70286c8b967aef8da29959
RichardKim <hiddeneventor@gmail.com>
2021-06-20 21:09:52 +0900

    from 전송 학습 완료

--- vue-cli/src/components/Login.vue
+++ vue-cli/src/components/Login.vue
@@ -1,16 +1,57 @@
 <template lang="">
-  <form action="">
+  <form v-on:submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input type="text" id="username" v-model="username" />
+      <input
+        type="text"
+        id="username"
+        v-on:change="changeVal"
+        v-model="username"
+      />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="password" id="password" v-model="password" />
+      <input
+        type="password"
+        id="password"
+        v-on:change="changeVal"
+        v-model="password"
+      />
     </div>
     <button>login</button>
   </form>
 </template>
 <script>
-export default {};
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    changeVal(e) {
+      console.log(e.target.name);
+      console.log(e.target.value);
+    },
+  },
+};
 </script>
