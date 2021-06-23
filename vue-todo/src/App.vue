b316ae4f36e32d2177127089f0fed29b15c2d20c
RichardKim <hiddeneventor@gmail.com>
2021-06-23 22:52:11 +0900

    v-model, LocalStorage 저장

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -22,6 +22,9 @@ export default {
     TodoList
   }
 }
+
+
+
 </script>
 
 <style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    input
+    <input type="text" v-model="newTodoItem">
+    <button v-on:click="addTodo">add</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoInput"
+  data: function (){
+    return  {
+      newTodoItem: ""
+    }
+  },
+  methods: {
+    addTodo: function (){
+      console.log(this.newTodoItem)
+      // 저장하는 로직 ( 로컬 스토리지)
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      // 초기화
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
