ffc8a48ab6c99109dd7234ba08f829f5f7227256
RichardKim <hiddeneventor@gmail.com>
2021-06-23 23:29:14 +0900

    List에서 item 삭제 하기

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <ul>
-      <li v-for="todoItem in todoItems" v-bind:key="todoItem">
-        {{ todoItem}}
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem">
+        {{ todoItem }}
+        <span v-on:click="removeTodo(todoItem, index)">
+         <i class="fas fa-trash-alt"></i>
+        </span>
       </li>
     </ul>
   </div>
@@ -10,20 +13,26 @@
 
 <script>
 export default {
-  data(){
+  data() {
     return {
       todoItems: []
     }
   },
+  methods: {
+    removeTodo(todoItem, index) {
+      console.log(todoItem, index)
+      localStorage.removeItem(todoItem)
+      this.todoItems.splice(index, 1);
+    }
+  },
   created() {
-    if(localStorage.length > 0){
-      for (let i = 0; i < localStorage.length; i++){
-        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
           this.todoItems.push(localStorage.key(i))
         }
       }
     }
-
   }
 }
 </script>
