38993857169dec1e34a03748d02ec96532906afb
RichardKim <hiddeneventor@gmail.com>
2021-07-07 00:03:09 +0900

    모달 common component 추가 Slot 학습 및 테스트

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -2,14 +2,30 @@
   <div>
     <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
     <button v-on:click="addTodo">add</button>
+    <!-- 모달 적용-->
+    <Modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">
+        내용을 추가합니다
+      </h3>
+      <h3 slot="body">
+        바디내용 재정의
+      </h3>
+      <h2 slot="footer">
+        하단 내용 재정의
+      </h2>
+    </Modal>
+    <!-- 모달 적용-->
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal";
+
 export default {
   data: function (){
     return  {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     }
   },
   methods: {
@@ -18,11 +34,16 @@ export default {
         this.$emit('addTodoItem', this.newTodoItem)
         // 초기화
         this.clearInput()
+      }else{
+        this.showModal = !this.showModal;
       }
     },
     clearInput: function () {
       this.newTodoItem = '';
     }
+  },
+  components:{
+    Modal:Modal
   }
 }
 </script>
